f46d929a6d5c28b436f23f969ff31ae0aaf91705
Shahul <skhajamo@ubuntu.(none)>
2012-03-24 23:40:23 -0700

    removed comments2

--- prefix_sum.cpp
+++ prefix_sum.cpp
@@ -197,8 +197,8 @@ int main()
 
 	gettimeofday(&tz, &tx);
 	start_time = (double)tz.tv_sec + (double) tz.tv_usec / 1000000.0;
-	for(int i=0;i<MAX_NUM;i++)
-		checkArr[i+1] += checkArr[i];
+	for(int i=1;i<MAX_NUM;i++)
+		checkArr[i] += checkArr[i-1];
 	gettimeofday(&tz, &tx);
 	end_time = (double)tz.tv_sec + (double) tz.tv_usec / 1000000.0;
 	double stime = ((double)end_time - (double)start_time);
